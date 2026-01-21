3c1d2b6bac4a94a661ab4bb982b5020c059ecb02
paramveersingh01 <paramveer.singh@kodytechnolab.com>
2026-01-21 11:26:27 +0530

    set orientation to portrait only

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nearbys/ui/home_screen/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
